255b46bee7dc077a4e5e7f983c0a6d4746eb85f8
Sebastian C <sebscoolmail@gmail.com>
2018-04-28 17:28:29 -0500

    Added double free test case

---
+++ test/basic/hello_dblfree.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../../src/shim.h"
+
+int main()
+{
+  start_mtrace_fork_server();
+  puts("Hello World!");
+
+  void *a = malloc(4);
+  free(a);
+  free(a);
+
+  return 0;
+}
